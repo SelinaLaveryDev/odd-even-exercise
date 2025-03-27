32f4427be086c49417dd0674f679901795635b52
Selina Lavery <selina118@hotmail.com>
2025-03-27 09:48:26 +0000

    initial js including query selectors, test dom manipulation and counter variable

---
+++ script.js
@@ -0,0 +1,20 @@
+console.clear();
+
+// query selectors
+const counterButton = document.querySelector(".counter-button");
+const oddColumn = document.querySelector(".odd-column");
+const oddBox = document.createElement("div");
+const boxNum = document.createElement("h1");
+
+//variables
+let counter = 0;
+
+// dom manipulation
+oddBox.classList.add("box");
+oddColumn.appendChild(oddBox);
+oddBox.appendChild(boxNum);
+boxNum.classList.add("box-numbers");
+boxNum.innerText = "1";
+console.log(boxNum);
+
+//event listeners
