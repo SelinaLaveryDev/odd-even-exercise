78630f8d1fdf5f2951cb4be0b431ab52da136bf3
Selina Lavery <selina118@hotmail.com>
2025-03-27 10:57:20 +0000

    dom manipulators removed and createBox function added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.clear();
 
 // query selectors
 const counterButton = document.querySelector(".counter-button");
+const counterText = document.querySelector(".counter-text");
 const oddColumn = document.querySelector(".odd-column");
 const oddBox = document.createElement("div");
 const boxNum = document.createElement("h1");
@@ -9,13 +10,18 @@ const boxNum = document.createElement("h1");
 //variables
 let counter = 0;
 
-// dom manipulation
-oddBox.classList.add("box");
-oddColumn.appendChild(oddBox);
-oddBox.appendChild(boxNum);
-boxNum.classList.add("box-numbers");
-boxNum.innerText = "1";
-console.log(boxNum);
+// functions
+function createBox() {
+  const div = document.createElement("div");
+  div.classList.add("box");
+  // number the box
+  const h1 = document.createElement("h1");
+  h1.classList.add("box-numbers");
+  h1.innerText = counter;
+  div.appendChild(h1);
+  console.log("div box", div);
+  addBoxToColumn(div);
+}
 
 //event listeners
 counterButton.addEventListener("click", function () {
